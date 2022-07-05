62a00c73598ffc828036d236cdf26229c5bcac5e
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-05 16:26:25 -0400

    updates server code

---
+++ parser.rb
@@ -0,0 +1,22 @@
+def parse(request)
+  method, path, version = request.lines[0].split
+  {
+    path: path,
+    method: method,
+    headers: parse_headers(request)
+  }
+end
+def parse_headers(request)
+  headers = {}
+  request.lines[1..-1].each do |line|
+    return headers if line == "\r\n"
+    header, value = line.split
+    header        = normalize(header)
+    headers[header] = value
+  end
+  def normalize(header)
+    header.gsub(":", "").downcase.to_sym
+  end
+end
+
+parse(request)
--- server.rb
+++ server.rb
@@ -8,3 +8,33 @@ loop {
 
   puts request
 }
+
+# def parse(request)
+#   method, path, version = request.lines[0].split
+
+#   {
+#     path: path,
+#     method: method,
+#     headers: parse_headers(request)
+#   }
+# end
+
+# def parse_headers(request)
+#   headers = {}
+
+#   request.lines[1..-1].each do |line|
+#     return headers if line == "\r\n"
+
+#     header, value = line.split
+#     header        = normalize(header)
+
+#     headers[header] = value
+#   end
+
+#   def normalize(header)
+#     header.gsub(":", "").downcase.to_sym
+#   end
+# end
+
+# puts parse(request)
+# puts parse_headers(request)
